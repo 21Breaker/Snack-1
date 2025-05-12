f516ac32869f9f83cded96dd864da30cc75feb7a
21Breaker <jhuttsell2@ivytech.edu>
2025-05-12 00:10:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// Export App as the default export so that environments like Snack can render it.
+export default App;
